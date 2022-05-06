c3628373b7647688a179981e66311ee9c3aa94e4
Xanderelsmith@gmail.com <xanderelsmith@gmail.com>
2022-05-06 22:25:36 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,193 @@
+import 'package:flutter/material.dart';
+
+import 'package:parse_server_sdk/parse_server_sdk.dart';
+
+void main() async {
+  const KappId = '5XPbK9z51XXEY7oDcTQcoeZ3sBhi8dqmGqCc7LZl';
+const KclientKey = 'R7619iHmHWtCA1M1Co8CQy1t89yIqq4y2xyGumRS';
+const keyParseServerUrl = 'https://parseapi.back4app.com';
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Parse().initialize(KappId, keyParseServerUrl,
+      clientKey: KclientKey, autoSendSessionId: true);
+  var object = ParseObject('firstClass')
+    ..set('message', 'Hi ,this is my first connection to back4app');
+  await object.save();
+  print('saved succesfulluy');
+  runApp(MaterialApp(
+    home: Home(),
+  ));
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  final todoController = TextEditingController();
+
+  void addToDo() async {
+    if (todoController.text.trim().isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text("Empty title"),
+        duration: Duration(seconds: 2),
+      ));
+      return;
+    }
+    await saveTodo(todoController.text);
+    setState(() {
+      todoController.clear();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Parse Todo List"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+              padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: TextField(
+                      autocorrect: true,
+                      textCapitalization: TextCapitalization.sentences,
+                      controller: todoController,
+                      decoration: InputDecoration(
+                          labelText: "New todo",
+                          labelStyle: TextStyle(color: Colors.blueAccent)),
+                    ),
+                  ),
+                  ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        onPrimary: Colors.white,
+                        primary: Colors.blueAccent,
+                      ),
+                      onPressed: addToDo,
+                      child: Text("ADD")),
+                ],
+              )),
+          Expanded(
+              child: FutureBuilder<List<ParseObject>>(
+                  future: getTodo(),
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.none:
+                      case ConnectionState.waiting:
+                        return Center(
+                          child: Container(
+                              width: 100,
+                              height: 100,
+                              child: CircularProgressIndicator()),
+                        );
+                      default:
+                        if (snapshot.hasError) {
+                          return Center(
+                            child: Text("Error..."),
+                          );
+                        }
+                        if (!snapshot.hasData) {
+                          return Center(
+                            child: Text("No Data..."),
+                          );
+                        } else {
+                          return ListView.builder(
+                              padding: EdgeInsets.only(top: 10.0),
+                              itemCount: snapshot.data!.length,
+                              itemBuilder: (context, index) {
+                                //*************************************
+                                //Get Parse Object Values
+                                final varTodo = snapshot.data![index];
+                                final varTitle = varTodo.get<String>('title')!;
+                                final varDone = varTodo.get<bool>('done')!;
+                                //*************************************
+
+                                return ListTile(
+                                  title: Text(varTitle),
+                                  leading: CircleAvatar(
+                                    child: Icon(
+                                        varDone ? Icons.check : Icons.error),
+                                    backgroundColor:
+                                        varDone ? Colors.green : Colors.blue,
+                                    foregroundColor: Colors.white,
+                                  ),
+                                  trailing: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      Checkbox(
+                                          value: varDone,
+                                          onChanged: (value) async {
+                                            await updateTodo(
+                                                varTodo.objectId!, value!);
+                                            setState(() {
+                                              //Refresh UI
+                                            });
+                                          }),
+                                      IconButton(
+                                        icon: Icon(
+                                          Icons.delete,
+                                          color: Colors.blue,
+                                        ),
+                                        onPressed: () async {
+                                          await deleteTodo(varTodo.objectId!);
+                                          setState(() {
+                                            final snackBar = SnackBar(
+                                              content: Text("Todo deleted!"),
+                                              duration: Duration(seconds: 2),
+                                            );
+                                            ScaffoldMessenger.of(context)
+                                              ..removeCurrentSnackBar()
+                                              ..showSnackBar(snackBar);
+                                          });
+                                        },
+                                      )
+                                    ],
+                                  ),
+                                );
+                              });
+                        }
+                    }
+                  }))
+        ],
+      ),
+    );
+  }
+
+  Future<void> saveTodo(String title) async {
+    final todo = ParseObject('Todo')
+      ..set('title', title)
+      ..set('done', false);
+    await todo.save();
+  }
+
+  Future<List<ParseObject>> getTodo() async {
+    QueryBuilder<ParseObject> queryTodo =
+        QueryBuilder<ParseObject>(ParseObject('Todo'));
+    final ParseResponse apiResponse = await queryTodo.query();
+
+    if (apiResponse.success && apiResponse.results != null) {
+      return apiResponse.results as List<ParseObject>;
+    } else {
+      return [];
+    }
+  }
+
+  Future<void> updateTodo(String id, bool done) async {
+    var todo = ParseObject('Todo')
+      ..objectId = id
+      ..set('done', done);
+    await todo.save();
+  }
+
+  Future<void> deleteTodo(String id) async {
+    var todo = ParseObject('Todo')..objectId = id;
+    await todo.delete();
+  }
+}
